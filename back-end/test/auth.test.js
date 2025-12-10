62405cc23fb861bf31b2429adb8f3dfc648e859e
m9membreno <160680473+m9membreno@users.noreply.github.com>
2025-12-09 23:36:40 -0500

    Update auth.test.js

--- back-end/test/auth.test.js
+++ back-end/test/auth.test.js
@@ -18,7 +18,7 @@ describe('Auth API Tests - Sprint 3', () => {
     this.timeout(10000)
     
     // Use TEST database (not production!)
-    const testDbUri = process.env.MONGODB_TEST_URI
+    const testDbUri = process.env.MONGODB_UNIT_TEST_URI
     
     // Close existing connection if any
     if (mongoose.connection.readyState !== 0) {
